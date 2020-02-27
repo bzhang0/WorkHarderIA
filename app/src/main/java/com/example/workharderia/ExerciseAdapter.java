4974915cb60aa4f3fa600e9f3817a53f91fc556a
therealgalaxi <42699471+therealgalaxi@users.noreply.github.com>
2020-02-27 03:45:55 -0800

    v0.4
    SQLite implementation

---
+++ app/src/main/java/com/example/workharderia/ExerciseAdapter.java
@@ -0,0 +1,62 @@
+package com.example.workharderia;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseAdapter.ExerciseViewHolder> {
+    private Context mContext;
+    private Cursor mCursor;
+
+    public ExerciseAdapter(Context context, Cursor cursor) {
+        mContext = context;
+        mCursor = cursor;
+    }
+
+    public class ExerciseViewHolder extends RecyclerView.ViewHolder {
+        public TextView exerciseText;
+        public TextView repCountText;
+        public TextView weightCountText;
+
+
+        public ExerciseViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            exerciseText = itemView.findViewById(R.id.textview_name_exercise);
+            repCountText = itemView.findViewById(R.id.textview_reps_exercise);
+            weightCountText = itemView.findViewById(R.id.textview_weight_exercise);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ExerciseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.item_display_workout, parent, false);
+        return new ExerciseViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExerciseViewHolder holder, int position) {
+        if (!mCursor.moveToPosition(position)) {
+            return;
+        }
+
+        String name = mCursor.getString(mCursor.getColumnIndex(WorkoutContract.WorkoutEntry.COLUMN_EXERCISENAME));
+        int reps = mCursor.getInt(mCursor.getColumnIndex(WorkoutContract.WorkoutEntry.COLUMN_REPS));
+        int weight = mCursor.getInt(mCursor.getColumnIndex(WorkoutContract.WorkoutEntry.COLUMN_WEIGHT));
+
+        holder
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/example/workharderia/StartWorkout.java
+++ app/src/main/java/com/example/workharderia/StartWorkout.java
@@ -2,19 +2,30 @@ package com.example.workharderia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class StartWorkout extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
-    private String selectedFocusArea;
+    private SQLiteDatabase mDatabase;
+
+    private String passedSelectedFocusArea;
     private TextView text_selectedFocusAreaTitle;
 
+    private Button button_selectWorkoutIndicator;
+
+    private TextView mTextViewWeight;
+    private TextView mTextViewReps;
+    private int mWeight = 0;
+    private int mReps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +33,121 @@ public class StartWorkout extends AppCompatActivity implements PopupMenu.OnMenuI
         setContentView(R.layout.activity_start_workout);
 
         Intent intent = getIntent();
-        selectedFocusArea = intent.getStringExtra("com.example.workharderia.FOCUSAREA");
+        passedSelectedFocusArea = intent.getStringExtra("com.example.workharderia.FOCUSAREA");
+
+
+        ExerciseDBHelper dbHelper = new ExerciseDBHelper(this);
+        mDatabase = dbHelper.getWritableDatabase();
 
         text_selectedFocusAreaTitle = (TextView) findViewById(R.id.text_selectedFocusAreaTitle);
-        text_selectedFocusAreaTitle.setText("selected focus area: " + selectedFocusArea);
+        text_selectedFocusAreaTitle.setText("selected focus area: " + passedSelectedFocusArea);
+
+        button_selectWorkoutIndicator = (Button) findViewById(R.id.button_selectWorkoutIndicator);
+        mTextViewWeight = (TextView) findViewById(R.id.weight_amountDisplayText);
+        mTextViewReps = (TextView) findViewById(R.id.rep_amountDisplayText);
+
+        Button weight_buttonIncrease = findViewById(R.id.weight_add);
+        Button weight_buttonDecrease = findViewById(R.id.weight_subtract);
+
+        Button rep_buttonIncrease = findViewById(R.id.rep_add);
+        Button rep_buttonDecrease = findViewById(R.id.rep_subtract);
+
+        Button buttonInsertExercise = findViewById(R.id.insert_exercise);
+
+        weight_buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weightIncrease();
+            }
+        });
+
+        weight_buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weightDecrease();
+            }
+        });
+
+        rep_buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                repIncrease();
+            }
+        });
+
+        rep_buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                repDecrease();
+            }
+        });
+
+        buttonInsertExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertExercise();
+            }
+        });
+    }
+
+    private void weightIncrease() {
+        mWeight += 5;
+        mTextViewWeight.setText(String.valueOf(mWeight));
+    }
+
+    private void weightDecrease() {
+        if (mWeight > 0) {
+            mWeight -= 5;
+            mTextViewWeight.setText(String.valueOf(mWeight));
+        }
+    }
+
+    private void repIncrease() {
+        mReps++;
+        mTextViewReps.setText(String.valueOf(mReps));
+    }
+
+    private void repDecrease() {
+        if (mReps > 0) {
+            mReps--;
+            mTextViewReps.setText(String.valueOf(mReps));
+        }
+    }
+
+    private void insertExercise() {
+        String exerciseName = button_selectWorkoutIndicator.getText().toString();
+        if (exerciseName.equals("select workout") || mReps == 0) {
+            return;
+        }
+
+        ContentValues cv = new ContentValues();
+        cv.put(WorkoutContract.WorkoutEntry.COLUMN_EXERCISENAME, exerciseName);
+        cv.put(WorkoutContract.WorkoutEntry.COLUMN_WEIGHT, mWeight);
+        cv.put(WorkoutContract.WorkoutEntry.COLUMN_REPS, mReps);
+
+        mDatabase.insert(WorkoutContract.WorkoutEntry.TABLE_NAME, null, cv);
     }
 
     public void showPopup(View v) {
         PopupMenu popup = new PopupMenu(this, v);
         popup.setOnMenuItemClickListener(this);
-        popup.inflate(R.menu.popup_menu);
+        if (passedSelectedFocusArea.equals("arms")) {
+            popup.inflate(R.menu.workout_menu_arms);
+        } else if (passedSelectedFocusArea.equals("back")) {
+            popup.inflate(R.menu.workout_menu_back);
+        } else if (passedSelectedFocusArea.equals("chest")) {
+            popup.inflate(R.menu.workout_menu_chest);
+        } else if (passedSelectedFocusArea.equals("core")) {
+            popup.inflate(R.menu.workout_menu_core);
+        } else { // legs
+            popup.inflate(R.menu.workout_menu_legs);
+        }
+        popup.show();
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
-        return false;
+        button_selectWorkoutIndicator.setText(item.getTitle().toString());
+        return true;
     }
 }
